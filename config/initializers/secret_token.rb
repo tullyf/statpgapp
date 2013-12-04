374499adb5b3c818f87a84ffcce3dc60b65290e3
Frank Tully <tullyf1963@gmail.com>
2013-12-04 12:34:00 -0500

    Stat app first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statpgapp::Application.config.secret_token = 'd68969edc05d733052bafb371bcc2f3d92bd5001452e64becdf6be775a67293e047fa458fb395a0effd73613a2e4d922ff41a16b083386984444e84bcb14a0fd'
